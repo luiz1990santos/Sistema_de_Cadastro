82fb6dd3acba629eaf53a57ae5478f548b77b7f9
Luiz dos Santos <67069686+luiz1990santos@users.noreply.github.com>
2022-07-19 17:45:37 -0300

    Update mysql_cadastro.py

--- mysql_cadastro.py
+++ mysql_cadastro.py
@@ -3,7 +3,7 @@ import mysql.connector
 banco = mysql.connector.connect(
    host= 'localhost',
     user='root',
-    passwd='2403120818'
+    passwd=''
 )# Variável com od dados pare conectar ao banco.
 
 cursor = banco.cursor()
